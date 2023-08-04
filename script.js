8dee58a63a2ccfdf60e55ed11a7d98eabcea15ee
swaraj735 <76740003+swaraj735@users.noreply.github.com>
2023-08-04 21:16:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+// In this template, the script.js file is empty as there is no specific functionality to add.
+// You can include JavaScript code here if you want to add interactivity to the website.
